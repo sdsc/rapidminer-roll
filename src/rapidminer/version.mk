af7e93c743f7c72e16114b3c4bbca0b6d63be943
Trevor Cooper <tcooper@sdsc.edu>
2013-11-21 15:38:40 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/rapidminer/version.mk
+++ src/rapidminer/version.mk
@@ -1,3 +1,15 @@
-NAME    = rapidminer
-VERSION = 5.2.008
-RELEASE = 1
+NAME               = rapidminer
+VERSION            = 5.2.008
+RELEASE            = 1
+PKGROOT            = /opt/rapidminer
+
+SRC_SUBDIR         = rapidminer
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tgz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TGZ_PKGS           = $(SOURCE_PKG)
+
