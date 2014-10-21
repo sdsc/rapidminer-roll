08796dc83ef1da1abac935b717f28efa4c4dea20
Jim Hayes <jhayes@sdsc.edu>
2014-10-21 14:19:05 -0700

    Upgrade version.

--- src/rapidminer/version.mk
+++ src/rapidminer/version.mk
@@ -1,16 +1,16 @@
 NAME           = rapidminer
-VERSION        = 5.2.008
-RELEASE        = 1
+VERSION        = 6.1.0
+RELEASE        = 0
 PKGROOT        = /opt/rapidminer
 
 SRC_SUBDIR     = rapidminer
 
-SOURCE_NAME    = rapidminer
-SOURCE_SUFFIX  = tgz
+SOURCE_NAME    = rapidminer-studio
+SOURCE_SUFFIX  = zip
 SOURCE_VERSION = $(VERSION)
 SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
-SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+SOURCE_DIR     = $(SOURCE_NAME)
 
-TGZ_PKGS       = $(SOURCE_PKG)
+ZIP_PKGS       = $(SOURCE_PKG)
 
 RPM.EXTRAS     = AutoReq:No
